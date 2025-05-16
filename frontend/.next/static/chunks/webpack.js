087fead6c629cc79da162d9cdd3ad35a45cfc2c4
Bluzora-glitch <bluzolarteam@gmail.com>
2025-05-17 06:26:19 +0700

    backend import data

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b6a126fb90509dd2"; }
+/******/ 		__webpack_require__.h = function() { return "1b686f182e4b664c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
