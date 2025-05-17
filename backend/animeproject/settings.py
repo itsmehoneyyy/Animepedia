9b2f8fb904307615cbef5c59d06a0f9661258563
Bluzora-glitch <bluzolarteam@gmail.com>
2025-05-17 21:37:22 +0700

    Initial commit

--- backend/animeproject/settings.py
+++ backend/animeproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rlzqlfb^d=w#y)@jlp_776+2n11jc540ia-)7j=w$_zo-+&03c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = True
