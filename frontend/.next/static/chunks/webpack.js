00cdb4e29b62e3bfee7a0d4a7b7ec0833f395ab2
Bluzora-glitch <bluzolarteam@gmail.com>
2025-05-18 03:13:43 +0700

    initial commit

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62a441861905753b"; }
+/******/ 		__webpack_require__.h = function() { return "d4a0c8c9283e0ecb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
